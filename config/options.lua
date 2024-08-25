80ce9bca7c5a1c05d8d1fe1d622c27717c00690a
eugene-whitaker <eugene.whitaker25@gmail.com>
2024-08-24 20:25:09 -0400

    Add basic options, keymaps, and autocommands

---
+++ config/options.lua
@@ -0,0 +1,59 @@
+-- [[ Setting Options ]]
+-- See `:help vim.opt :help option-list`
+
+-- Set relative line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set mouse support to all modes
+vim.opt.mouse = 'a'
+
+-- Unset show mode
+vim.opt.showmode = false
+
+-- Set clipboard to sync between OS and Neovim
+-- See `:help clipboard`
+vim.schedule(function()
+    vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Set break indent
+vim.opt.breakindent = true
+
+-- Set undo history
+vim.opt.undofile = true
+
+-- Set case-insensitive searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Set draw signcolumn to always
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Set split open directions
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Set display for whitespace characters
+-- See `:help list :help listchars`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Set substitutions previews
+vim.opt.inccommand = 'split'
+
+-- Set show the line the cursor is on
+vim.opt.cursorline = true
+
+-- Set number of lines to keep above and below the cursor
+vim.opt.scrolloff = 10
+
+-- Set tab size
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
--- init.lua
+++ init.lua
@@ -1,1 +1,23 @@
-print('hello world')
+-- Set <SPACE> as the leader key
+-- See `:help mapleader`
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Unset Nerd Font support
+vim.g.have_nerd_font = false
+
+require('config/options')
+require('config/keymaps')
+require('config/autocmds')
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+-- See `:help lazy.nvim.txt`
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+	error('Error cloning lazy.nvim:\n' ..out)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
